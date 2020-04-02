1153dd68aee1cdde105d7f4a96c25228777216f0
ainguyen <40418101+QuynhAi@users.noreply.github.com>
2020-04-02 01:22:52 -0700

    adding navigation menu

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,6 +18,7 @@
  * 
 */
 
+let currentActiveSection = null;
 
 /**
  * End Global Variables
@@ -49,6 +50,50 @@
 */
 
 // Build menu 
+document.body.onload = function(){
+    addMenuBar();
+    document.onmouseover = function(event){
+        //console.log(event.target.nodeName);
+        if (event.target.nodeName == "SECTION"){
+            console.log(event.target.parentNode.id);
+
+            var active = document.getElementsByClassName("your-active-class");
+            var activeId = document.getElementById(active[0].id);
+            activeId.classList.remove("your-active-class");
+            //debugger;
+            var newActive = document.getElementById(event.target.parentNode.id);
+            newActive.classList.add("your-active-class");
+        }
+    }
+};
+
+function addMenuBar () { 
+    let current = document.getElementById("navbar__list"); 
+    let sectionLength = document.querySelectorAll('section').length;
+    for (let i = 1; i < sectionLength + 1; i++){
+        let li = document.createElement("li");  
+        let a = document.createElement('a');
+        let linkText = document.createTextNode("Section " + i);
+        a.appendChild(linkText);
+        //a.id = "section"+i;
+        //a.title = "my title text";
+        a.href = "#section"+i;      
+        a.onclick = function(event){
+            //console.log(event.target.hash);
+            var active = document.getElementsByClassName("your-active-class");
+            var activeId = document.getElementById(active[0].id);
+            activeId.classList.remove("your-active-class");
+            //debugger;
+            var newActive = document.querySelector(event.target.hash);
+            newActive.classList.add("your-active-class");
+            //console.log(active[0].id);
+        } 
+        li.appendChild(a);  
+        current.append(li); 
+    }
+}
+
+
 
 // Scroll to section on link click
 
