be71c9ca60df71754533352d8d46735527866ef7
ainguyen <40418101+QuynhAi@users.noreply.github.com>
2020-04-07 21:22:19 -0700

    project

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,40 @@
-// Setup empty JS object to act as endpoint for all routes
+/* Empty JS object to act as endpoint for all routes */
 projectData = {};
-
-// Require Express to run server and routes
-
-// Start up an instance of app
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// TODO-Express to run app and routes
+const express = require('express');
+// TODO-Start up an instance of app
+const app = express(); // object of the express module
+/* Dependencies */
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-
-// Initialize the main project folder
+// TODO-Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+/* Initializing the main project folder */
 app.use(express.static('website'));
+const port = 8080;
 
+//Create a local app with Node and Express:
+app.listen(port,listening);
 
-// Setup Server
+function listening(){
+    console.log("app running");
+    console.log('running on localhost ' + port);
+}
+
+let data = [];
+app.get('/all', function(req,res){
+    res.send({projectData});
+});
+
+app.post('/weather', function(req, res){ 
+    let newEntry={
+        temp: req.body.temp,
+        content: req.body.contenta,
+    }
+    data.push(newEntry);
+    res.send(data);
+    console.log("received:" + data);
+});
+
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,61 @@
 /* Global Variables */
+// 'api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}'
+let baseUrl = 'https://www.api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '26660429b6fea85fec7bc8d0b05f6555';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(){
+    const zipcode =  document.getElementById('zip').value;
+    const feeling =  document.getElementById('feelings').value;
+    getDate(zipcode)
+    .then(function(data){
+        postData('/weather', {temp: data.main.temp, content: data.weather[0].description});
+    }).then(updateUI())
+}
+
+
+const getDate = async(zipcode)=>{ 
+    const url = baseUrl + zipcode + ",us&appid=" + apiKey;
+    const res = await fetch(url);
+    try{
+        const data = await res.json();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+const postData = async(url='', data={})=>{
+    const res = await fetch(url,{
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    });
+    console.log('Reach middle of client post data');
+    //2.
+    try{
+        const newData = await res.json();
+        console.log('In client post data',newData);
+
+        return newData;
+    }catch(error){
+        console.log(`error ${error}`);
+    }
+}
+
+const updateUI = async () => {
+    let d = new Date();
+    let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+    const request = await fetch('/all');
+    try{
+      const allData = await request.json();
+      console.log('In update UI',allData);
+      document.getElementById('date').innerHTML = newDate;
+      document.getElementById('temp').innerHTML = allData.temp;
+      document.getElementById('content').innerHTML = allData.content; 
+    }catch(error){
+      console.log("error", error);
+    }
+  }
